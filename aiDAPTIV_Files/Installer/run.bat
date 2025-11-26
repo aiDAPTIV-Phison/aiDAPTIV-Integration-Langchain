56b805f53f3dda8c4929d15a4f81c9ff08155c2a
KinhouMaiStorage <kinhou.chan@maistorage.com>
2025-11-26 09:13:42 +0800

    Add installer docs, new installer main.py, and update run.bat

--- aiDAPTIV_Files/Installer/run.bat
+++ aiDAPTIV_Files/Installer/run.bat
@@ -1,17 +1,17 @@
 @echo off
-REM Change current directory to where the batch file is located
+:: make the folder that contains this bat the current directory
 pushd "%~dp0"
 
-REM Set environment variables
+:: ---- environment variables ----
 set "OPENAI_BASE_URL=http://localhost:8080/v1"
 set "OPENAI_MODEL=model"
-set "EXAMPLE_DOCS_FILE_DIR=%~dp0..\Example\Files\"
+:: absolute path to the PDF directory
+set "EXAMPLE_DOCS_FILE_DIR=%~dp0docs\"
 
-REM Define path to your embeddable python.exe (relative to the batch file)
-set "PY_EMBED=%~dp0..\python-3.10\python.exe"
+:: ---- paths to the shipped interpreter and script ----
+set "PY_EMBED=%~dp0python-3.10\python.exe"
+set "MAIN_PY=%~dp0main.py"
 
-REM Run your Python script (also relative to the batch file)
-"%PY_EMBED%" "%~dp0..\main.py"
-
-REM Return to the original directory
+:: ---- launch ----
+"%PY_EMBED%" "%MAIN_PY%"
 popd
